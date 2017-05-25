d6924b1edb1a1a67c11c7a8d3f53f29a2a9be5c4
peter <peter@foo-bar-baz-bif.local>
2017-05-24 17:41:03 -0700

    Fixed indent an merge conflicts.

--- main.erl
+++ main.erl
@@ -4,16 +4,20 @@
 
 worldEventLoop() ->
 	receive
-		% {badarg,[{io,format,[<0.48.0>,"Player '~s' has arrived.~n",<<109,105,108,111,10>>],[]}
+    % {badarg,[{io,format,[<0.48.0>,"Player '~s' has arrived.~n",<<109,105,108,111,10>>],[]}
 		{newplayer, PlayerName} -> io:format("Player '~s' has arrived.~n", [PlayerName]);
 		{lostplayer, PlayerName} -> io:format("Player '~s' has departed.~n", [PlayerName])
 	end,
 	worldEventLoop().
 
 start() ->
-  R = room:load_map("map.dat"),
-  R ! {self(), enter, player1},
-  R ! {self(), get, players},
+  {Worked, [R|_]} = room:load_map("map.dat"),
+  case Worked of
+    ok -> ok;
+    _ -> throw("Map load failed")
+  end,
+  %% R ! {self(), enter, player1},
+  %% R ! {self(), get, players},
   io:format("World initialized.~n"),
   spawn(net, listenForClients, [self()]),
 	io:format("Listening for clients -- server ready.~n"),
--- room.erl
+++ room.erl
@@ -6,22 +6,22 @@
 -include("room_interface.hrl").
 
 new(Room) ->
-    Result = receive
-                 {Pid, get} ->
-                     Pid ! {room_data, Room};
-                 {_, enter, Player} ->
-                     Room#room_data{players = [Player | Room#room_data.players]};
-                 {_, exit, Player} ->
-                     NewPlayers = lists:filter(fun(P) -> P /= Player end),
-                     Room#room_data{players = NewPlayers}
-             end,
-    case Result of
-        ok -> new(Room);
-        NewRoom -> new(NewRoom)
-    end.
+  Result = receive
+             {Pid, get} ->
+               Pid ! {room_data, Room};
+             {_, enter, Player} ->
+               Room#room_data{players = [Player | Room#room_data.players]};
+             {_, exit, Player} ->
+               NewPlayers = lists:filter(fun(P) -> P /= Player end),
+               Room#room_data{players = NewPlayers}
+           end,
+  case Result of
+    ok -> new(Room);
+    NewRoom -> new(NewRoom)
+  end.
 
 load_map(Filename) ->
-    file:consult(Filename).
-        
+  file:consult(Filename).
+
 save_map(Filename, RootRoom) ->
-    file:write_file(Filename, io_lib:format("~p.~n", [RootRoom])).
+  file:write_file(Filename, io_lib:format("~p.~n", [RootRoom])).
