34116f72565b3043e90bba11fcc53cebde634504
Milo Trujillo <milo.trujillo@daylightingsociety.org>
2017-05-24 20:07:01 -0700

    Added instructions, better help

--- net.erl
+++ net.erl
@@ -1,6 +1,7 @@
 -module(net).
 -export([listenForClients/1]).
 -include("configuration.hrl").
+-include("constants.hrl").
 
 -define(TCP_OPTIONS, [binary, {active, false}, {reuseaddr, true}]).
 
@@ -19,7 +20,7 @@ clientLogin(Sock, World) ->
 	gen_tcp:send(Sock, "Login: "),
 	case gen_tcp:recv(Sock, 0) of
 		{ok, RawUsername} ->
-			Username = re:replace(RawUsername, "\\s+", "", [global,{return,list}]),
+			Username = string:to_lower(re:replace(RawUsername, "\\s+", "", [global,{return,list}])),
 			World ! {newplayer, Username},
 			gen_tcp:send(Sock, ?BANNER),
 			clientLoop(Sock, Username, World);
@@ -39,16 +40,13 @@ clientLoop(Sock, Username, World) ->
 		{tcp_error, Sock, Reason} ->
 			io:format("Error on socket ~p reason: ~p~n", [Sock, Reason]),
 			World ! {lostplayer, Username};
-		{tcp, Sock, <<"help", _/binary>>} -> 
-			gen_tcp:send(Sock, "The Lord helps those who help themselves.\n"),
-			clientLoop(Sock, Username, World);
 		{tcp, Sock, Msg} ->
 			parseCommand(Sock, Username, World, strings:rstrip(Msg)),
 			clientLoop(Sock, Username, World)
 	end.
 
 parseCommand(Sock, Username, World, Line) ->
-	case string:tokens(Line, " ") of
+	case strings:lcTokens(Line, " ") of
 		["go", Direction] ->
 			io:format("'~s' is traveling '~s'~n", [Username, Direction]),
 			World ! {travel, Username, Direction, self()},
@@ -69,6 +67,10 @@ parseCommand(Sock, Username, World, Line) ->
 				{error, Description} ->
 					gen_tcp:send(Sock, "Something has gone wrong -- cannot get room description\n")
 			end;
+		["help"] ->
+			gen_tcp:send(Sock, [?INSTRUCTIONS, "\n"]);
+		["who", "am", "i"] ->
+			gen_tcp:send(Sock, ["Your name is ", Username, "\n"]);
 		_Else ->
 			gen_tcp:send(Sock, [getRandomDidNotUnderstand(), "\n"]),
 			io:format("Stuck parsing: '~p'~n", [string:tokens(Line, " ")])
